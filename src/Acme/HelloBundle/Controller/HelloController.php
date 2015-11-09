f2e975d2f38b2cbfee2572956ae47c5ffeef0fb2
Bastubacka <benjamin.colas@gmail.com>
2015-11-09 21:44:39 +0000

    hello world ++

--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -9,10 +9,15 @@
 // src/Acme/HelloBundle/Controller/HelloController.php
 namespace Acme\HelloBundle\Controller;
 
-class HelloController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HelloController extends Controller
 {
         public function indexAction($name)
     {
-        return new Response('<html><body>Hello '.$name.'!</body></html>');
+        //return new Response('<html><body>Hello '.$name.'!</body></html>');
+        return $this->render(
+                'AcmeHelloBundle:Hello:index.html.twig', 
+                array('name' => $name));
     }
 }
